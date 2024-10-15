2f6902c2554d47c547636d97bf2eafee70718b20
Giovanni <usr3855@salesianos-lacuesta.net>
2024-10-15 18:49:01 +0100

    mediana

--- src/net/salesianos/metodos/Mediana.java
+++ src/net/salesianos/metodos/Mediana.java
@@ -1,5 +1,22 @@
 package net.salesianos.metodos;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Mediana {
-    
+    public static void main(String[] args) {
+
+        String num = args[0];
+        String[] mediana = num.split(",");
+        Collections.sort(num);
+        double formediana;
+        int cantidad = num.length();
+        if (cantidad % 2 == 0) {
+            mediana = (num[cantidad / 2 - 1] + num[cantidad / 2]) / 2;
+        } else {
+            mediana = num[cantidad / 2];
+        }
+
+    }
 }
+
