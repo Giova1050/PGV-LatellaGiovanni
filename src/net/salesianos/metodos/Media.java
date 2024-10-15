bd5e047d188b85d913e50afb7aacdbfffdcfcd33
Giovanni <usr3855@salesianos-lacuesta.net>
2024-10-15 18:47:41 +0100

    media

--- src/net/salesianos/metodos/Media.java
+++ src/net/salesianos/metodos/Media.java
@@ -1,5 +1,17 @@
 package net.salesianos.metodos;
 
+import java.util.ArrayList;
+
 public class Media {
-    
+    public static void main(String[] args) {
+
+        String num = args[0];
+        String[] media = num.split(",");
+        double suma = 0;
+        for (double fornum : num) {
+            suma += fornum;
+        }
+        double resultado = suma / num.length();
+    }
 }
+
