3409427c304e0a9484111a79d85ba1ac01bbff61
Giovanni <usr3855@salesianos-lacuesta.net>
2024-10-15 18:49:51 +0100

    launcher

--- src/net/salesianos/procesos/ProcessLauncher.java
+++ src/net/salesianos/procesos/ProcessLauncher.java
@@ -1,5 +1,22 @@
 package net.salesianos.procesos;
 
+import java.io.File;
+import java.io.IOException;
+
 public class ProcessLauncher {
-    
+    private static final String NUMBER_CALCULATE_ROUTE = "./src/net/salesianos/metodos/Calculos.java";
+
+    public static Process initMedia(String nums, String outputFileName) throws IOException {
+        ProcessBuilder builder = new ProcessBuilder("java", NUMBER_CALCULATE_ROUTE, nums); 
+        builder.redirectOutput(new File("./outputs/files/" + outputFileName));
+        builder.redirectError(new File("./outputs/errors/numberCalculator.txt"));
+        return builder.start();
+    }
+
+    public static Process initMediana(String nums, String outputFileName) throws IOException {
+        ProcessBuilder builder = new ProcessBuilder("java", NUMBER_CALCULATE_ROUTE, nums); 
+        builder.redirectOutput(new File("./outputs/files/" + outputFileName));
+        builder.redirectError(new File("./outputs/errors/numberCalculator.txt"));
+        return builder.start();
+    }
 }
