60ccf2b29e13c7aac0c6bb5388e99bb06b4b9bc2
Giovanni <usr3855@salesianos-lacuesta.net>
2024-10-15 18:51:10 +0100

    app

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
+import java.io.File;
+import java.util.ArrayList;
+import net.salesianos.procesos.ProcessLauncher;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String lines = "input\\Datos.csv";
+
+        ArrayList<Process> calculateProcesses = new ArrayList<>();
+
+        for (int i = 0; i < lines.size(); i++) {
+            Process process = ProcessLauncher.initMedia(lines.get(i), "calculus-" + i + ".txt");
+            calculateProcesses.add(process);
+        }
+
+        for (Process process : calculateProcesses) {
+            process.waitFor();
+        }
+
+        for (int i = 0; i < lines.size(); i++) {
+            Process process = ProcessLauncher.initMediana(lines.get(i), "calculus-" + i + ".txt");
+            calculateProcesses.add(process);
+        }
+
+        for (Process process : calculateProcesses) {
+            process.waitFor();
+        }
+
+        
     }
 }
+
